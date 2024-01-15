420d84c88772a7b2df6130b62328e429b6b5ed29
Eduardo Dos Santos Gomes <eddos-sa@c2r11p3.42sp.org.br>
2024-01-15 18:53:04 -0300

    aa

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: eddos-sa <eddos-sa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 15:50:03 by eddos-sa          #+#    #+#             */
-/*   Updated: 2024/01/11 18:11:19 by eddos-sa         ###   ########.fr       */
+/*   Updated: 2024/01/15 18:52:09 by eddos-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,18 +32,21 @@ typedef struct s_minishell
 	char			**path;
 	char			*execute_path;
 	struct s_token	*token;
+	struct s_validation *validations;
 }					t_minishell;
 
 typedef struct s_token
 {
 	char			*content;
 	int				type;
+	char **args;
 	struct s_token	*next;
 	struct s_token	*previous;
 }					t_token;
 
 int					ft_isalldigit_minishell(const char *str);
-int					validate_input(const char *token, char **envp, t_minishell *mini);
+int					validate_input(const char *token, char **envp,
+						t_minishell *mini);
 int					commands(char *token, char **env, t_minishell *mini);
 char				*validate_cmd(t_minishell *mini, char *token);
 void				get_path(char **env, t_minishell *mini);
--- src/main.c
+++ src/main.c
@@ -1,21 +1,81 @@
 #include "../include/minishell.h"
 
-void	minishell(const char *str, t_minishell *mini, char **envp)
+void	add_token(char *str, int type, t_minishell *mini)
 {
-	if (validate_input(str, envp, mini) == 0)
-		return ;
-}
-void	print_tokens(t_token *token_list)
-{
-	t_token	*current_token;
+	t_token	*new_token;
+	t_token	*last_token;
 
-	current_token = token_list;
-	while (current_token != NULL)
+	new_token = malloc(sizeof(t_token));
+	if (!new_token)
+		exit(EXIT_FAILURE);
+	if (ft_strlen(str) == 2)
+		new_token->content = ft_strdup(str);
+	else
+		new_token->content = str;
+	new_token->type = type;
+	new_token->next = NULL;
+	if (mini->token)
 	{
-		printf("Content: %s, Type: %d\n", current_token->content,
-			current_token->type);
-		current_token = current_token->next;
+		last_token = mini->token;
+		while (last_token->next != NULL)
+			last_token = last_token->next;
+		last_token->next = new_token;
 	}
+	else
+		mini->token = new_token;
+}
+
+int	check_meta(const char *input, t_minishell *mini)
+{
+	int	i;
+
+	i = 0;
+	while (input[i])
+	{
+		if (input[i] == '>' && input[i + 1] == '>')
+		{
+			add_token(">>", DOUBLE_GREAT, mini);
+			i++;
+		}
+		else if (input[i] == '<' && input[i + 1] == '<')
+		{
+			add_token("<<", DOUBLE_LESS, mini);
+			i++;
+		}
+		else if (input[i] == '>' && input[i + 1] != '>')
+			add_token(">", GREAT, mini);
+		else if (input[i] == '<' && input[i + 1] != '<')
+			add_token("<", LESS, mini);
+		else if (input[i] == '|')
+			add_token("|", PIPE, mini);
+		i++;
+	}
+	return (1);
+}
+
+void	check_quote(const char *prompt, t_minishell *mini)
+{
+	int	quote;
+	int	double_quote;
+
+	quote = 0;
+	double_quote = 0;
+	while (*prompt)
+	{
+		if (*prompt == '"')
+			double_quote++;
+		if(*prompt == '\'')
+			quote++;
+		prompt++;
+		
+	}
+}
+
+void	lexer(const char *prompt, t_minishell *mini)
+{
+	check_meta(prompt, mini);
+	check_quote(prompt, mini);
+	check_space(prompt, mini);
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -30,7 +90,14 @@ int	main(int argc, char **argv, char **envp)
 	while (1)
 	{
 		prompt = readline("Minishell $> ");
-		minishell(prompt, mini, envp);
-		print_tokens(mini->token);
+		lexer(prompt, mini);
 	}
 }
+
+/* 
+Tokenização:
+Terminar o seu trabalho com as aspas
+~Validação de aspas (tá fechado, aspa dentro de aspa etc)
+Adaptar a função para funcionar apenas foras das aspa, se caso tiver dentro imprimir como se fosse string
+
+*/
