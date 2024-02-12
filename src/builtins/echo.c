aed0269be47c70517b517ed7c317bd002ad9763a
Jaque <jaque_hp@ghotmail.com>
2024-02-11 23:57:48 -0300

    starting implement echo

---
+++ src/builtins/echo.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By:  jaqribei <jaqribei@student.42.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/11 23:57:02 by  jaqribei         #+#    #+#             */
+/*   Updated: 2024/02/11 23:57:08 by  jaqribei        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+
+// mimics the behavior of the echo command in a Unix shell.
+// Need to make the command list =, and fix n_flag to work with a variable number of n's
+
+void	ft_echo(t_minishell *mini)
+{
+	int		n_flag;
+	int		i;
+	char	**args;
+
+	args = mini->cmd->args;
+	n_flag = 0;
+	i = 0;
+	if (!args)
+		ft_putstr_fd("\n", 1);
+	else
+	{
+		while (args[i] && ft_strncmp(args[i], "-n", 2) == 0)
+		{
+			n_flag = 1;
+			i++;
+		}
+		while (args[i] != NULL)
+		{
+			ft_putstr_fd(args[i], 1);
+			if (args[i + 1] != NULL)
+				ft_putstr_fd(" ", 1);
+			i++;
+		}
+	}
+	if(n_flag == 0)
+		ft_putstr_fd("\n", 1);
+}
