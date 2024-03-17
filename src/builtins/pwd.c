6b25c2d46c2d5dbac293f9a420f383d652c30cd2
Edu2metros <eduardosantos14.gomes@gmail.com>
2024-03-17 14:24:50 -0300

    pwd

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -6,29 +6,31 @@
 /*   By: eddos-sa <eddos-sa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 16:20:21 by jaqribei          #+#    #+#             */
-/*   Updated: 2024/03/17 12:29:33 by eddos-sa         ###   ########.fr       */
+/*   Updated: 2024/03/17 14:23:48 by eddos-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void	ft_pwd(void)
+void    ft_pwd(t_cmd *cmd)
 {
-	char	*pwd;
+    char    *pwd;
+    int        fd_out;
 
-	pwd = getcwd(NULL, 0);
-	if (pwd)
-	{
-		ft_putstr_fd(pwd, 1);
-		ft_putstr_fd("\n", 1);
-	}
-	else
-	{
-		ft_printf_fd(STDERR_FILENO, "minishell: pwd error\n");
-		get_control()->return_status = 1;
-	}
-	get_control()->return_status = 0;
-	free(pwd);
-	if(get_control()->cmd->on_fork == 1)
-		exit(get_control()->return_status);
+    fd_out = 1;
+    cmd->redirect_list_out = lstlast_out(cmd->redirect_list_out);
+    if (cmd->redirect_list_out)
+        fd_out = cmd->redirect_list_out->fd_out;
+    pwd = getcwd(NULL, 0);
+    if (pwd)
+        ft_printf_fd(fd_out, "%s\n", pwd);
+    else
+    {
+        ft_printf_fd(STDERR_FILENO, "minishell: pwd error\n");
+        get_control()->return_status = 1;
+    }
+    get_control()->return_status = 0;
+    free(pwd);
+    if(get_control()->cmd->on_fork == 1)
+        exit(get_control()->return_status);
 }
