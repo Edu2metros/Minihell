225ceb7f4701a2b6f6e717f1201adde36214278a
Jaqueline Ribeiro Inocencio <jaqribei@student.42sp.org.br>
2024-03-17 14:55:20 -0300

    pipe with redirect

--- src/execution/pipes.c
+++ src/execution/pipes.c
@@ -30,18 +30,21 @@ void	fd_redirections(int fd_in, int fd_out)
 void	handle_child_process(t_cmd *cmd, t_minishell *mini, int fd[], int fd_in)
 {
 	close(fd[0]);
-	if (cmd->next != NULL)
+	if (cmd->next != NULL && !cmd->redirect_list_out)
 	{
 		fd_redirections(STDIN_FILENO, fd[1]);
 		fd_redirections(fd_in, STDOUT_FILENO);
 	}
+	else if (cmd->next != NULL && cmd->redirect_list_out)
+	{
+		cmd->redirect_list_out = lstlast_out(cmd->redirect_list_out);
+		fd_redirections(STDIN_FILENO, fd[1]);
+		fd_redirections(fd_in, cmd->redirect_list_out->fd_out);
+	}
 	else
 		fd_redirections(fd_in, STDOUT_FILENO);
 	if (is_builtin(cmd->name))
-	{
-		cmd->on_fork = 1;
 		builtin_execution(cmd, mini);
-	}
 	else
 		exec_pipe_command(cmd, mini);
 	exit(EXIT_SUCCESS);
@@ -64,6 +67,11 @@ void	exec_pipe(t_minishell *mini, t_cmd *cmd)
 	pid_t	pid;
 	int		status;
 
+	if (cmd->redirect_list_in)
+	{
+		cmd->redirect_list_in = lstlast_in(cmd->redirect_list_in);
+		dup2(cmd->redirect_list_in->fd_in, STDIN_FILENO);
+	}
 	fd_in = STDIN_FILENO;
 	count = lstsize_cmd(cmd);
 	while (cmd)
